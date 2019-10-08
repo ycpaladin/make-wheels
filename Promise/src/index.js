c0b6a22254155a8fe840f4993addf68834643e07
ycpaladin <ycpaladin@126.com>
2019-10-08 22:08:26 +0800

    添加部分代码注释

--- Promise/src/index.js
+++ Promise/src/index.js
@@ -7,6 +7,7 @@ const isError = is('[object Error]')
 /**
  * 自定义Promise对象
  * @param {(resolve: (value: any) => void, reject: (reason: any) => void) => void} executor 
+ * @this {MyPromise}
  */
 function MyPromise(executor) {
   if (!(this instanceof MyPromise)) {
@@ -17,8 +18,19 @@ function MyPromise(executor) {
   this.status = 'pending'
 
   /**
+   * @type {(onfulfilled: (value: any) => void) => any}
+   */
+  this.onfulfilled = null
+
+  /**
+   * @type {(onrejected: (reason: any) => void) => Error}
+   */
+  this.onrejected = null
+
+  /**
    * 内部判定状态，只有处于pending状态才可继续执行
    * @param {number} value 
+   * @this {MyPromise}
    */
   function resolve(value) {
     if (this.status === 'pending') { // 确保只执行一次
@@ -29,6 +41,8 @@ function MyPromise(executor) {
   /**
    * 为了缓存resolve方法的执行结果
    * @param {*} value value就是resolve方法的执行结果
+   * @this {MyPromise}
+   * @returns {(onfulfilled: (value: any) => void) => any}
    */
   function ___onfulfilled(value) {
     this.status = 'fulfilled'   // 更改内部状态
@@ -43,6 +57,7 @@ function MyPromise(executor) {
   /**
    * 触发异常的方法
    * @param {string} reason 
+   * @this {MyPromise}
    */
   function reject(reason) {
     if (this.status === 'pending') {  // 确保只执行一次
@@ -53,6 +68,7 @@ function MyPromise(executor) {
   /**
    * 
    * @param {Error} reason 如果传入的不是一个Error对象，那么会使用Error包装一下
+   * @this {MyPromise}
    */
   function ___onrejected(reason) {
     this.status = 'rejected'    // 更改内部状态
@@ -64,10 +80,14 @@ function MyPromise(executor) {
 
 
   /**
+   * @this {MyPromise}
    * @param {(value: number) => any} onfulfilled 处理成功的函数
    * @param {(reason: any) => void} onrejected 处理出现异常的函数，如果传入该参数，那么catch方法就捕获不到了
    */
   this.then = function(onfulfilled, onrejected) {
+    /**
+     * @type {MyPromise}
+     */
     const self = this
     return new MyPromise((resolve, reject) => {
       setTimeout(function waitStatus() {
@@ -100,8 +120,12 @@ function MyPromise(executor) {
   /**
    * 捕获异常
    * @param {(reason: any) => void} onrejected
+   * @this {MyPromise}
    */
   this.catch = function(onrejected) {
+    /**
+     * @type {MyPromise}
+     */
     const self = this
     setTimeout(function reject() {
       if (self.status === 'rejected') {
